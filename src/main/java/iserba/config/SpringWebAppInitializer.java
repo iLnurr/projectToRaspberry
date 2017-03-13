a3b0e40cca00ed272361bdac92fbf4c785efe431
ilnur <ilnur.serbaev@dz.ru>
2017-03-13 12:53:03 +0500

    return active profiles - hibernate and postgres

--- src/main/java/iserba/config/SpringWebAppInitializer.java
+++ src/main/java/iserba/config/SpringWebAppInitializer.java
@@ -17,7 +17,7 @@ public class SpringWebAppInitializer extends AbstractAnnotationConfigDispatcherS
         appContext.register(ApplicationContextConfig.class);
 
         final ConfigurableEnvironment env = appContext.getEnvironment();
-        env.setActiveProfiles("h2", "jdbc");
+        env.setActiveProfiles("hibernate", "postgres");
 
         appContext.refresh();
 
